d333c0afad0fcc8ba8034d9b19bfde119016c106
YURI JUNG <dbfl14433@gmail.com>
2022-12-19 01:48:02 +0900

    채팅도전 중

---
+++ src/main/java/com/example/chat/config/MessagingStompWebsocketApplication.java
@@ -0,0 +1,9 @@
+package com.example.chat.config;
+
+import org.springframework.boot.SpringApplication;
+
+public class MessagingStompWebsocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/chat/controller/controller.java
@@ -0,0 +1,26 @@
+package com.example.chat.controller;
+
+import com.example.chat.dto.Greeting;
+import com.example.chat.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class controller {
+
+    @RequestMapping("/productWrite")
+    public String yuriBoardWrite() throws Exception {
+        return "yuri";
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
